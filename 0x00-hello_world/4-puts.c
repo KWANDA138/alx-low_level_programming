ce824bad490183c8870f12adba795044e6c8c6c7
KWANDA138 <thandomtimande138@gmail.com>
2023-02-26 12:20:53 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+/**
+ * main -a program that prints a line with the puts function
+ *
+ * Return: always 0 (success)
+ *
+ */int main(void)
+(
+puts (\completed: 0.0%)
+Write a C program that prints exactly "Programming is like building a multilingual puzzle)
+return(0);
+)
