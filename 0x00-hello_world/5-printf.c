c0f1077dfe6f356490ff21e15de7a590cd489941
KWANDA138 <thandomtimande138@gmail.com>
2023-06-16 07:13:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,7 @@
 *main - A program that prints a line using the printf function
 *Return: 0 (success)
 */
-int main(void)
-{
-printf(with proper grammer, but the outcome is a piece of art, \n")
+int main(void);
+printf("with proper grammer, but the outcome is a piece of art, \n");
 return (0);
-}A
-
+}
