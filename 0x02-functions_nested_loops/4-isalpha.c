7cf4014399313f02ae020af7bcbcecddd962c662
KWANDA138 <thandomtimande138@gmail.com>
2023-06-21 15:02:36 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * 4-isalpha-checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int 4-isalpha(intc)
+{
+	return((c>='a'&&c<='z') ||(c'A'&&c<='Z'));
+}
